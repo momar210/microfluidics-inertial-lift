0bf9fa7b7a15b263b2acb4c360ed09e0f2625c6b
williamstoy <ws2574@columbia.edu>
2022-02-14 12:32:15 -0500

    Add comments to particle tracking scheme file

--- microfluidic-simulation_files/user_files/particle-tracking-repeated-steps-with-bootstrap.scm
+++ microfluidic-simulation_files/user_files/particle-tracking-repeated-steps-with-bootstrap.scm
@@ -1,10 +1,22 @@
 ;; REPEATED PARTICLE TRACKING
+;; Inertial microfluidic devices often have several repeated segments
+;; to modify fluid flow and particle positions within a channel.
+;; Meshing and simulating the entire device would be computationally expensive
+;; and applying periodic boundary conditions assumes the device is infinitely long.
+;; 
+;; To simulate particle flow in an inertial microfluidic device with finite repeated steps,
+;; we first release particles at the inlet from an initial injection DPM definition.
+;; Particles are then sampled at the outlet plane and re-injected in subsequent steps.
+
+
 ;; DEFINITIONS
-(define ninjections 2)
+(define ninjections 30)
+
 
 ;; RUN THE INITIAL INJECTION
 (display (format #f "\n\n--------------------\nINITIAL INJECTION\n--------------------\n\n"))
 (ti-menu-load-string (format #f "/report/dpm-sample initial-injection () outlet () () no"))
+
 
 ;; LOOP THE INJECTIONS
 (DO ((x 1 (+ 1 x))) ((> x (- ninjections 1)))
